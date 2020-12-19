b6c20dd75034b8633ff575e74801e60b8c38e288
Aditya Prakash <prakashadityaap@gmail.com>
2020-12-19 15:22:48 +0530

    Added voice method

--- app/src/main/java/com/example/speechrecognitionproject/MainActivity.java
+++ app/src/main/java/com/example/speechrecognitionproject/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -14,11 +15,14 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private static final int SPEAK_REQUEST = 10;
+    TextView txtValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtValue = (TextView) findViewById(R.id.txtValue);
 
         PackageManager packageManager = this.getPackageManager();
 
@@ -29,5 +33,13 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(MainActivity.this, "Device does not support speech recognition", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void listenToVoice(){
+        Intent voiceIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        voiceIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Listening!");
+        voiceIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        voiceIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 10);
+        startActivityForResult(voiceIntent, SPEAK_REQUEST);
     }
 }
